9fddc96d4bf471f0ea5f1711b1db85046931fe20
Aditya Srivastava <pks85715@gmail.com>
2025-11-08 22:41:30 +0530

    add the event card component and button components

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,26 @@
+import ExploreBtn from "@/components/ExploreBtn";
+import EventCard from "@/components/EventCard";
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <>
-   </>
+    <>
+      <section>
+        <h1 className="text-center"> The space for every dev <br /> Event you can not miss</h1>
+        <p className="text-center mt-5">Hackathons , Meetups , and Conferences , Everything in one place </p>
+        <ExploreBtn />
+        <div className="mt-20 space-y-7">
+          <h3>Featured Events</h3>
+
+          {/* <ul className="events">
+            {events && events.length > 0 && events.map((event: IEvent) => (
+              <li key={event.title} className="list-none">
+                <EventCard {...event} />
+              </li>
+            ))}
+          </ul> */}
+        </div>
+      </section>
+    </>
   );
 }
